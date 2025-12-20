6a8f764997a6e4f3b4f26f5a9eab5a2ace37def5
Victor Merino <victor.merino@vidneo.tc>
2025-12-20 20:56:26 +0100

    added root imports

--- src/claybird/__init__.py
+++ src/claybird/__init__.py
@@ -1,1 +1,4 @@
 from claybird.app.claybird import Claybird
+from claybird.infrastructure.adapters.inbound.http.routing.controller import Controller
+from claybird.infrastructure.adapters.inbound.http.routing.mappers import GetMapping, PostMapping, DeleteMapping, PostMapping, PatchMapping
+from claybird.infrastructure.adapters.outbound.dependencies.dict_dependency_container import DictDependencyContainer
